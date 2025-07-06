de6fefd0045b88261931eb10a010b3564c420d0e
alexchtst-01 <acinatra@gmail.com>
2025-07-06 22:26:53 +0700

    minor fix

--- src/lib/idGen.tsx
+++ src/lib/idGen.tsx
@@ -3,11 +3,10 @@ export default function GenerateId(salter: number = 7) {
 
     const day = String(time.getDate()).padStart(2, "0");
     const month = String(time.getMonth() + 1).padStart(2, "0");
-    const year = String(time.getFullYear());
     const hours = String(time.getHours()).padStart(2, "0");
     const minutes = String(time.getMinutes()).padStart(2, "0");
 
-    const timestamp = `${day}${month}${year}${hours}${minutes}`;
+    const timestamp = `${day}${month}${hours}${minutes}`;
 
     const rdID = Math.floor(Math.random() * (parseInt(timestamp) + salter));
 
