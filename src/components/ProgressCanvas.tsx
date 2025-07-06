95076868b17135be74d0461c61460b33a49f891b
alexchtst-01 <acinatra@gmail.com>
2025-07-07 03:29:34 +0700

    feat: refactor ProgressCanvas to accept tasks as props and improve layout

--- src/components/ProgressCanvas.tsx
+++ src/components/ProgressCanvas.tsx
@@ -3,53 +3,28 @@
 import React from 'react';
 import { useDroppable } from '@dnd-kit/core';
 
-import { Priority, Progress, Task } from "@/types/task";
+import { Progress, Task } from "@/types/task";
 import TaskComponent from "./TaskComponent";
-import GenerateId from '@/lib/idGen';
-
-const tasks: Task[] = [
-    {
-        id: GenerateId(),
-        title: "Write Unit Tests",
-        description: "Add tests for critical components and utility functions.",
-        progress: Progress.DONE,
-        priority: Priority.LOW
-    },
-    {
-        id: GenerateId(),
-        title: "Optimize Performance",
-        description: "Audit the app and fix performance bottlenecks.",
-        progress: Progress.IN_PROGRESS,
-        priority: Priority.MEDIUM
-    },
-    {
-        id: GenerateId(),
-        title: "Deploy to Production",
-        description: "Push the stable build to the production environment.",
-        progress: Progress.TODO,
-        priority: Priority.HIGH
-    }
-];
-
 
 interface CompProps {
+    tasks: Task[]
     progress: Progress
     total: number;
 }
 
-export default function ProgressCanvas({ progress, total = 0 }: CompProps) {
-    const { isOver, setNodeRef } = useDroppable({
+export default function ProgressCanvas({ tasks, progress, total = 0 }: CompProps) {
+    const [openField, setOpenField] = React.useState(false);
+
+    const { setNodeRef } = useDroppable({
         id: progress,
     });
-    const style = {
-        opacity: isOver ? 0.8 : 1,
-    };
+
     return (
         <div
-            ref={setNodeRef} style={style}
-            className="w-[30%] h-[35vw] rounded-md shadow-md bg-gray-100 hover:shadow-lg p-5"
+            ref={setNodeRef}
+            className="w-[30%] min-h-[35vw] max-h-fit rounded-md shadow-md bg-gray-200 hover:shadow-lg flex flex-col justify-between p-5"
         >
-            <p className="pl-5 space-x-2 pb-5">
+            <div className="pl-5 space-x-2 pb-5">
                 <span
                     className={
                         `p-1 bg-gray-300 rounded-sm text-sm font-bold
@@ -62,16 +37,32 @@ export default function ProgressCanvas({ progress, total = 0 }: CompProps) {
                     {progress}
                 </span>
                 <span>{total}</span>
-            </p>
-            <div className="p-1 space-y-1 invisible-scrollbar overflow-y-scroll h-[80%]">
+            </div>
+            <div className="p-1 space-y-1 invisible-scrollbar overflow-y-auto h-[80%]">
                 {tasks.map((task, taskIdx) =>
                     <TaskComponent key={taskIdx} data={task} />
                 )}
             </div>
-            {/* create field */}
-            <button className="mt-5 cursor-pointer">
-                Create
-            </button>
+            <div className='mt-5 space-y-2'>
+                <button className="cursor-pointer bg-blue-800 py-2 px-5 text-white rounded-md" onClick={() => { setOpenField(!openField) }}>
+                    Create
+                </button>
+                <div className={`${openField ? '' : 'hidden'} space-y-3`}>
+                    <form
+                        className='space-y-2 bg-white p-2 rounded-md'
+                        action=""
+                    >
+                        <input
+                            type="text" placeholder='Title' className='border w-full p-2 rounded-sm'
+                        />
+                        <textarea
+                            placeholder='input the description here'
+                            rows={5}
+                            className='border w-full p-2 rounded-sm'
+                        />
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
