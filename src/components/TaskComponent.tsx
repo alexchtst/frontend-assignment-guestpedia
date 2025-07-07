c1604c598deea22f44645273363fac3490345185
alexchtst-01 <acinatra@gmail.com>
2025-07-07 22:17:44 +0700

    feat: enhance task component with edit functionality and priority display

--- src/components/TaskComponent.tsx
+++ src/components/TaskComponent.tsx
@@ -6,12 +6,15 @@ import { CSS } from "@dnd-kit/utilities"
 import { Priority, Task } from "@/types/task"
 
 import { SquareCheck, ChevronsDown, ChevronUp, AlignJustify } from "lucide-react"
+import TaskContext from "@/context/Taskcontext"
 
 interface CompProps {
     data: Task;
 }
 
 export default function TaskComponent({ data }: CompProps) {
+    const { handleEditDatatask } = React.useContext(TaskContext);
+
     const { attributes, listeners, setNodeRef, transform } = useDraggable({
         id: data.id,
     });
@@ -20,29 +23,47 @@ export default function TaskComponent({ data }: CompProps) {
         transform: CSS.Translate.toString(transform),
         zIndex: transform ? 50 : 1,
         position: transform ? 'absolute' : 'static',
-        width: transform ? '90%' : '100%'
+        width: transform ? '88.5%' : '100%'
     };
 
     return (
         <div
-            className="bg-white p-4 space-y-3 cursor-grab"
             ref={setNodeRef}
             style={style}
-            {...listeners}
-            {...attributes}
         >
-            <p>{data.title}</p>
-            <div className="flex w-full justify-between items-center">
-                <div className="flex justify-center items-center space-x-1 text-[1vw]">
-                    <SquareCheck color="blue" width={15} />
-                    <p className="font-semibold text-gray-500">{data.id}</p>
+            <div className="bg-white p-4 space-y-3">
+                <div
+                    className="space-y-3 cursor-grab"
+                    {...listeners}
+                    {...attributes}
+                >
+                    <p>{data.title}</p>
+                    <div className="flex w-full justify-between items-center">
+                        <div
+                            className="flex justify-center items-center space-x-1 text-[1vw]">
+                            <SquareCheck color="blue" width={15} />
+                            <p className="font-semibold text-gray-500">{data.id}</p>
+                        </div>
+                        <div>
+                            {
+                                data.priority === Priority.MEDIUM ? <AlignJustify color="#EF9F08" width={20} /> :
+                                    data.priority === Priority.LOW ? <ChevronsDown color="blue" width={25} height={20} /> :
+                                        <ChevronUp color="red" width={25} height={20} />
+                            }
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    {
-                        data.priority === Priority.MEDIUM ? <AlignJustify color="#EF9F08" width={20} /> :
-                            data.priority === Priority.LOW ? <ChevronsDown color="blue" width={25} height={20} /> :
-                                <ChevronUp color="red" width={25} height={20} />
-                    }
+
+                <div className="flex items-center space-x-2">
+                    <button
+                        className="px-2 py-1 bg-yellow-300 text-[10px] rounded-lg font-semibold cursor-pointer hover:bg-yellow-500"
+                        onClick={() => { handleEditDatatask(data) }}
+                    >
+                        edit
+                    </button>
+                    <button className="px-2 py-1 bg-red-500 text-[10px] rounded-lg font-semibold cursor-pointer text-white hover:bg-red-800">
+                        delete
+                    </button>
                 </div>
             </div>
         </div>
