dd4ea6796d870c9d6687f823e2abe93a0b0d1ee2
alexchtst-01 <acinatra@gmail.com>
2025-07-08 17:42:38 +0700

    feat: add new TypeScript interfaces for Toaster and Canvas components include all the interface needed in every component

---
+++ src/types/ui.ts
@@ -0,0 +1,26 @@
+import { Progress, Task } from "./task";
+
+export interface ToasterProps {
+  title: string;
+  content: string;
+}
+
+export interface ToasterContextInterface {
+  openToaster: boolean;
+  toasterData: ToasterProps;
+  handleShow: (d: ToasterProps) => void;
+}
+
+export interface ComponenChildInterface {
+  children: React.ReactNode;
+}
+
+export interface PropgressCanvasProps {
+  tasks: Task[];
+  progress: Progress;
+  total: number;
+}
+
+export interface ComponentDataInterface {
+    data: Task;
+}
