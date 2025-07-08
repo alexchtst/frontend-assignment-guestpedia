8d08dcec98d079e6f93ba5dbbae2930b96aca7df
alexchtst-01 <acinatra@gmail.com>
2025-07-08 17:43:52 +0700

    feat: create toaster ui

--- src/components/GeneralToaster.tsx
+++ src/components/GeneralToaster.tsx
@@ -1,5 +1,27 @@
+'use client'
+
+import ToasterContext from "@/context/Toastercontext";
+import { useContext } from "react";
+
 export default function GeneralToaster() {
+    const { openToaster, toasterData } = useContext(ToasterContext);
+
     return (
-        <div>This is general Toaster</div>
+        <div
+            className={
+                `transition-all duration-500 ease-in-out 
+                ${openToaster ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-4 hidden'} 
+                fixed bottom-[2vw] right-[1vw] z-[100]`
+            }
+        >
+            <div className="flex justify-center items-center">
+                <div className="w-[32vw] h-[8vw] p-4 bg-white rounded-lg shadow-2xl space-y-2">
+                    <p className="text-sm font-bold">{toasterData.title}</p>
+                    <div className="text-[1.2vw]">
+                        {toasterData.content}
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
